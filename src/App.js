1a35dac392d2f4912a2ddce30e84150a13201272
Tom Dohnal <tomas.dohnal@gmail.com>
2020-11-27 16:49:00 +0100

    add animations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import AnimationReactSpring from "./AnimationReactSpring";
+import AnimationGSAP from "./AnimationGSAP";
+import AnimationFramerMotion from "./AnimationFramerMotion";
+
+const ANIMATIONS = [
+  {
+    element: <AnimationReactSpring />,
+    label: "react-spring",
+  },
+  {
+    element: <AnimationGSAP />,
+    label: "GSAP",
+  },
+  {
+    element: <AnimationFramerMotion />,
+    label: "framer-motion",
+  },
+];
 
 function App() {
+  const [animation, setAnimation] = useState(ANIMATIONS[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "16px" }}>
+      <div style={{ display: "flex" }}>
+        Pick animation library:
+        {ANIMATIONS.map((animation) => (
+          <button
+            key={`button-${animation.label}`}
+            onClick={() => {
+              setAnimation(animation);
+            }}
+          >
+            {animation.label}
+          </button>
+        ))}
+      </div>
+      <div>
+        <strong>{animation.label}</strong>
+      </div>
+      {animation.element}
     </div>
   );
 }
